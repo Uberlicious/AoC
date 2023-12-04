9479ebbaee6b49bc00d3d5546847d5ed766b679d
Uber <jevanmeader@gmail.com>
2023-12-04 14:01:21 -0600

    day-04 part 2 completed

--- 2023/day-04/src/bin/part2.rs
+++ 2023/day-04/src/bin/part2.rs
@@ -1,11 +1,91 @@
 fn main() {
-    let input = include_str!("./input2.txt");
+    let input = include_str!("./input1.txt");
     let output = process(input);
     dbg!(output);
 }
 
+#[derive(Debug)]
+struct Card {
+    id: u32,
+    amount: u32,
+    numbers: Vec<u32>,
+    winners: Vec<u32>,
+}
+
+impl Card {
+    fn new(id: u32, numbers: Vec<u32>, winners: Vec<u32>) -> Card {
+        Card {
+            id,
+            numbers,
+            winners,
+            amount: 1,
+        }
+    }
+
+    fn increment(&mut self, amount: u32) -> &mut Self {
+        self.amount += amount;
+        self
+    }
+}
+
+fn recurse(cards: &mut Vec<Card>, idx: usize, total: &mut u32) {
+    if idx >= cards.len() {
+        return;
+    }
+    let mut matches = 0;
+
+    for n in &cards[idx].numbers {
+        if cards[idx].winners.contains(&n) {
+            matches += 1
+        }
+    }
+
+    for m in cards[idx].id + 1..=cards[idx].id + matches {
+        let a = cards[idx].amount;
+        cards[m as usize - 1].increment(1 * a);
+    }
+
+    *total += cards[idx].amount;
+
+    recurse(cards, idx + 1, total);
+}
+
 fn process(input: &str) -> u32 {
-    0
+    let input = input.replace("\r\n", "\n");
+    let lines = &mut input.split("\n").collect::<Vec<_>>();
+    let mut cards: Vec<Card> = vec![];
+
+    let mut total: u32 = 0;
+
+    // get card data
+    for line in lines {
+        let game = line.split(":").collect::<Vec<_>>();
+        let game_id = game[0].split_ascii_whitespace().collect::<Vec<_>>()[1]
+            .parse::<u32>()
+            .expect("card id");
+
+        let nums = game[1]
+            .split("|")
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|n| {
+                return n
+                    .trim()
+                    .split_ascii_whitespace()
+                    .map(|n| n.trim().parse::<u32>().expect("num"))
+                    .collect::<Vec<u32>>();
+            })
+            .collect::<Vec<Vec<u32>>>();
+
+        let numbers = &nums[0];
+        let winners = &nums[1];
+
+        cards.push(Card::new(game_id, numbers.clone(), winners.clone()))
+    }
+
+    recurse(&mut cards, 0, &mut total);
+
+    total
 }
 
 #[cfg(test)]
@@ -13,7 +93,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_2() {
+    fn test_part_1() {
         let result = process(
             "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -22,6 +102,6 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
         );
-        assert_eq!(result, 13);
+        assert_eq!(result, 30);
     }
 }
